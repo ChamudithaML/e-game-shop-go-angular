54c6fcd3df4b29afbac8f99e2fb25fa48e946a35
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-10-28 11:06:02 +0530

    add game management microservice

---
+++ server/services/game_service/configs/setup.go
@@ -0,0 +1,36 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(GetMongoURI()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+var DB *mongo.Client = ConnectDB()
+
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("game-shop").Collection(collectionName)
+	return collection
+}
---
+++ server/services/game_service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	// "user_service/configs"
+	// "user_service/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	// configs.ConnectDB()
+	// routes.UserRoute(app)
+
+	app.Listen(":6000")
+}

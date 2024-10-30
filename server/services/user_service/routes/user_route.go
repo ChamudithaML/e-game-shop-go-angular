cdf8c75cfc9fc9981750440ad3ed88a179e21632
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-10-31 02:57:16 +0530

    minor changes

--- server/services/user_service/routes/user_route.go
+++ server/services/user_service/routes/user_route.go
@@ -2,11 +2,16 @@ package routes
 
 import (
 	"user_service/controllers"
+	// "user_service/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func UserRoute(app *fiber.App) {
 	app.Post("/signup", controllers.CreateUser)
+	app.Post("/login", controllers.LoginUser)
 
+	// Protected routes with jwt
+	// app.Get("/profile", middleware.AuthMiddleware, controllers.GetUserProfile)
+	// app.Put("/update", middleware.AuthMiddleware, controllers.UpdateUser)
 }

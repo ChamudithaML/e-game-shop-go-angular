0fa7b87e718d5200e8b4303666baa4bc38fd5997
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-11-15 15:23:34 +0530

    add services for game crud operations

---
+++ client/src/app/services/game.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+import { GameResponse } from '../models/game-response.model'; 
+import { Game } from '../models/game.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<GameResponse> {
+    return this.http.get<GameResponse>(environment.baseUrl);
+  }
+
+  create(data: Game): Observable<any> {
+    return this.http.post(environment.createUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${environment.baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${environment.baseUrl}/${id}`);
+  }
+
+}

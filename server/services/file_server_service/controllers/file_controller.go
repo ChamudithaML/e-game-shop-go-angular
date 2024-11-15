ccceec0d8abeeeeafd6bca842d7d37a884942d52
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-11-15 09:30:42 +0530

    add controller for the file service

---
+++ server/services/file_server_service/controllers/file_controller.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"file_server_service/models"
+	"file_server_service/services"
+	"log"
+)
+
+func CreateDocument(c *fiber.Ctx) error {
+	var letter models.LetterDocument
+
+	// Parse the JSON body into the LetterDocument struct
+	if err := c.BodyParser(&letter); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse JSON",
+		})
+	}
+
+	// Validate the struct using validate service
+	if err := services.ValidateLetterDocument(letter); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	// Generate the document
+	fileName, err := services.GenerateDocx(letter)
+	if err != nil {
+		log.Println("Error generating document:", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to generate document",
+		})
+	}
+
+	// Construct the file URL
+	fileURL := c.BaseURL() + "/docs/" + fileName
+
+	// Return the file URL
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":  "Document created successfully",
+		"file_url": fileURL,
+	})
+}

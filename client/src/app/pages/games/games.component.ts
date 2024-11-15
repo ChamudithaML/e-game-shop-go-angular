a6b3885cbfaa6b4ae3fc48a5bd3416866254bc8f
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-11-16 02:24:53 +0530

    add game data table component

---
+++ client/src/app/pages/games/games.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game.model';
+import { GameService } from '../../services/game.service';
+
+@Component({
+  selector: 'app-games',
+  templateUrl: './games.component.html',
+  styleUrls: ['./games.component.scss']
+})
+export class GamesComponent implements OnInit {
+
+  games: Game[] =[];
+  title = ''
+  displayAddForm: boolean = false;
+  displayEditForm: boolean = false;
+  selectedGame?: Game;
+
+  constructor(private gameService: GameService) { }
+
+  ngOnInit(): void {
+    this.retrieveGames();
+  }
+
+  // retrieveGames(): void {
+  //   this.gameService.getAll()
+  //     .subscribe({
+  //       next: (data) => {
+  //         this.games = data.data;
+  //         console.log(this.games);
+  //       },
+  //       error: (e) => console.error(e)
+  //     });
+  // }
+
+
+  retrieveGames(): void {
+    this.gameService.getAll().subscribe(response => {
+      this.games = response.data.data; 
+    }, error => {
+      console.error("Error retrieving games:", error); 
+    });
+  }
+
+  addGame():void{
+    console.log("Open add popup")
+    this.displayAddForm=true
+  }
+
+  reloadData():void{
+    this.displayAddForm=false
+    this.retrieveGames()
+  }
+
+  editGame(game:Game):void{
+    console.log("Open edit popup")
+    this.selectedGame = {...game}
+    this.displayEditForm=true
+  }
+
+  deleteGame(id:any):void{
+    this.gameService.delete(id)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (e) => console.error(e)
+      });
+    
+      console.log("sde")
+      this.retrieveGames()
+  }
+
+}

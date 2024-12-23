fd224e919fd0f7709b6276ba59d3cd9998fb53b2
Chamuditha <lilan.2019660@iit.ac.lk>
2024-12-23 23:16:42 +0530

    environment ts

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+
+    // game service urls
+    baseUrl: 'http://localhost:8082/games',
+    createUrl: 'http://localhost:8082/addgame',
+    proxyUrl: 'http://localhost:8080/proxy/game/games',
+    proxyUrlAdd: 'http://localhost:8080/proxy/game/addgame',
+
+    saleUrl:'http://localhost:8083/sales'
+  };
+  

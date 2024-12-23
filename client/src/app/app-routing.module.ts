a046842e6d307f2042326d164067da23c5ba8ee2
Chamuditha <lilan.2019660@iit.ac.lk>
2024-12-23 23:27:46 +0530

    changes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,17 +5,13 @@ import { GamesComponent } from './pages/games/games.component';
 import { PracticePageComponent } from './pages/practice-page/practice-page.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
-import { DynamicFormComponent } from './pages/dynamic-form/dynamic-form.component';
-import { DynamicFormTwoComponent } from './pages/dynamic-form-two/dynamic-form-two.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'dash', component: DashboardComponent },
   { path: 'games', component: GamesComponent },
-  { path: 'prac', component: PracticePageComponent },
-  { path: 'dyn', component: DynamicFormComponent },
-  { path: 'dyn2', component: DynamicFormTwoComponent }
+  { path: 'prac', component: PracticePageComponent }
 ];
 
 @NgModule({
